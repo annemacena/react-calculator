292ea690cfac50202d156baa63c95ec1cd12b134
Anne <arianne.macena@gmail.com>
2020-01-29 20:23:27 -0300

    updating build files

---
+++ build/precache-manifest.ee6b2dd66c241399db26b27a14f79938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f30ae5fe06faa608b1884175a72189f",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "e0622508e170b60eda35",
+    "url": "/react-calculator/static/css/main.5d4b3a40.chunk.css"
+  },
+  {
+    "revision": "e978d9dba70138e65a96",
+    "url": "/react-calculator/static/js/2.af6d7e53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator/static/js/2.af6d7e53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0622508e170b60eda35",
+    "url": "/react-calculator/static/js/main.a7361fd9.chunk.js"
+  },
+  {
+    "revision": "909d0137fa7114c15e08",
+    "url": "/react-calculator/static/js/runtime-main.a14a8bab.js"
+  },
+  {
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/react-calculator/static/media/RobotoMono-Thin.b93c2ad4.ttf"
+  }
+]);
